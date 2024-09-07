4e961d83aeda1f75013fd47d6c8df100ed49ea7f
Keenan Nicholson <keenanbnicholson@gmail.com>
2024-09-07 13:59:29 -0230

    initial setup and map

---
+++ src/App.tsx
@@ -0,0 +1,39 @@
+import { useEffect } from "react";
+import "./App.css";
+import maplibregl from "maplibre-gl";
+import "maplibre-gl/dist/maplibre-gl.css";
+
+const mapkey = import.meta.env.VITE_MAPTILER_KEY;
+
+function App() {
+  useEffect(() => {
+    // Initialize the map when the component mounts
+    const map = new maplibregl.Map({
+      container: "map", // container id
+      style: `https://api.maptiler.com/maps/streets/style.json?key=${mapkey}`, // style URL
+      center: [0, 0], // starting position [lng, lat]
+      zoom: 1, // starting zoom
+    });
+
+    map.addControl(
+      new maplibregl.GeolocateControl({
+        positionOptions: {
+          enableHighAccuracy: true,
+        },
+        trackUserLocation: true,
+      })
+    );
+
+    return () => {
+      map.remove();
+    };
+  }, []);
+
+  return (
+    <div>
+      <div id="map" style={{ width: "100%", height: "100vh" }}></div>
+    </div>
+  );
+}
+
+export default App;
